3f8bcdcf41aab568a107a4a2e0ac338f275796f3
naark <zakaraya.ilia@gmail.com>
2019-01-29 16:47:28 +0300

    Added command line args support; double-direction distance calc

--- task1.cpp
+++ task1.cpp
@@ -8,46 +8,74 @@
 std::vector<std::string> words;
 std::string instr;
 
-int main()
+int main(int argc, char *argv[])
 {
-	int min = 0;
-	int max = 0;
+	if (argc > 1)
+	{
+		int min = 0;
+		int max = 0;
 
-	//Открываем хэндл к файлу
-	std::ifstream textsrc("C:\\Users\\Haxxor\\Documents\\Visual Studio 2017\\Projects\\algotasks\\Debug\\textfile.txt");
-	if (textsrc.is_open())
-	{
-		//Добавляем слова в контейнер по отдельности
-		std::istream_iterator<std::string> input(textsrc), end;
-		std::copy(input, end, back_inserter(words));
-		//Дебаг
-		std::cout << "Amount of words: " << words.size() << std::endl;
-	}
-	
-	//Находим позицию слова "today"
-	auto pos = std::find(words.begin(), words.end(), "Today");
-	//Итерируем элементы контейнера со словами
-	for (std::vector<std::string>::iterator i = words.begin(); i != words.end(); i++)
-	{
-		//Сравниваем содержимое
-		if (!strcmp(i->c_str(), "day"))
+		std::string textPath(argv[1]);
+		std::string word1(argv[2]);
+		std::string word2(argv[3]);
+
+		// Открываем хэндл к файлу
+		std::ifstream textSrc(textPath);
+		if (textSrc.is_open())
 		{
-			//Если минимум 0, то задаём минимальную разницу в расстоянии между словами
-			if (min == 0)
-				min = max = std::distance(pos, i);
+			// Добавляем слова в контейнер по отдельности
+			std::istream_iterator<std::string> input(textSrc), end;
+			std::copy(input, end, back_inserter(words));
 			
-			//Каждое новое совпадение = максимальная разница
-			max = std::distance(pos, i);
+			// Дебаг
+			std::cout << "Amount of words: " << words.size() << std::endl;
 		}
+		
+		// Находим позицию первого слова
+		auto pos = std::find(words.begin(), words.end(), word1);
+
+		// Итерируем элементы контейнера со словами
+		for (std::vector<std::string>::iterator i = words.begin(); i != words.end(); i++)
+		{
+			// Сравниваем содержимое
+			// if (!strcmp(i->c_str(), word2.c_str()))
+			if ((*i).compare(word2) == 0)
+			{
+				std::cout << i - words.begin() << " / " << pos - words.begin() << std::endl;
+
+				if ((i - words.begin()) > pos - words.begin())
+				{
+					// Если минимум 0, то задаём минимальную разницу в расстоянии между словами
+					if (min == 0)
+					{
+						min = max = std::distance(pos, i);
+					}
+					
+					// Каждое новое совпадение = максимальная разница
+					max = std::distance(pos, i);
+				}
+				else
+				{
+					if (min == 0)
+					{
+						min = max = std::distance(i, pos);
+					}
+					
+					// Каждое новое совпадение = максимальная разница
+					max = std::distance(i, pos);
+				}
+				
+				
+			}
+		}
+
+		// -1, потому что distance [first:last)
+		std::cout << "Min distance: " << min - 1 << std::endl;
+		std::cout << "Max distance: " << max - 1 << std::endl;
+
+		// Закрываем хэндлы к файлу
+		textSrc.close();
 	}
-
-	//-1 потому что учитывается слово Today
-	//можно, конечно, считать со следующего слова, но это не комильфо
-	std::cout << "Min distance: " << min - 1 << std::endl;
-	std::cout << "Max distance: " << max - 1 << std::endl;
-
-	//Закрываем хэндлы к файлу
-	textsrc.close();
 
 	return 0;
 }
