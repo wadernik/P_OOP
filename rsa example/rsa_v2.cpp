81f87bf80e3a9d4f3a1501f7b1365f43d540bc56
developer.izakar.cbox <developer.izakar.cbox@gmail.com>
2022-02-25 16:12:54 +0300

    More indents fixes

--- rsa example/rsa_v2.cpp
+++ rsa example/rsa_v2.cpp
@@ -85,77 +85,77 @@ long int decrypt(long int c, long int &numberD, long int &modulePQ)
 
 int main()
 {
-	vector<double> encryptedChars;
+  vector<double> encryptedChars;
   vector<double> decryptedChars;
-	string text;
-	string key;
+  string text;
+  string key;
 
-	long int primeNumberP, primeNumberQ;
+  long int primeNumberP, primeNumberQ;
 
   // Произведение P * Q, модуль
-	long int modulePQ;
-	// Функция Эйлера
-	long int phi;
-	// Экспонента. Часть от пары открытого ключа
-	long int exponent;
-	// Часть от пары закрытого ключа
-	long int numberD;
+  long int modulePQ;
+  // Функция Эйлера
+  long int phi;
+  // Экспонента. Часть от пары открытого ключа
+  long int exponent;
+  // Часть от пары закрытого ключа
+  long int numberD;
 
-	// Читаем текст из файла и складывает в контейнер
-	ifstream textFile ("input.txt");
-	if (textFile.is_open())
-	{
-		getline(textFile, text);
-		textFile.close();
+  // Читаем текст из файла и складывает в контейнер
+  ifstream textFile ("input.txt");
+  if (textFile.is_open())
+  {
+    getline(textFile, text);
+    textFile.close();
 
-		cout << "Text: " << text << endl;
-	}
+    cout << "Text: " << text << endl;
+  }
 
-	// Читаем ключи из файла. Считаем, что в первой строке лежит P, во второй - Q.
-	ifstream keyFile ("keys.txt");
-	if (keyFile.is_open())
-	{
-		getline(keyFile, key);
+  // Читаем ключи из файла. Считаем, что в первой строке лежит P, во второй - Q.
+  ifstream keyFile ("keys.txt");
+  if (keyFile.is_open())
+  {
+    getline(keyFile, key);
 
-		primeNumberP = stol(key);
-		key.clear();
+    primeNumberP = stol(key);
+    key.clear();
 
-		getline(keyFile, key);
-		primeNumberQ = stol(key);
+    getline(keyFile, key);
+    primeNumberQ = stol(key);
 
-		keyFile.close();
+    keyFile.close();
 
     cout << "PrimeP: " << primeNumberP << endl;
     cout << "PrimeQ: " << primeNumberQ << endl;
-	}
+  }
 
   // Вычисляем публичный и закрытый ключ
-	// Вычисляем произведение P * Q
-	modulePQ = primeNumberP * primeNumberQ;
-	cout << "Mod: " << modulePQ << endl;
+  // Вычисляем произведение P * Q
+  modulePQ = primeNumberP * primeNumberQ;
+  cout << "Mod: " << modulePQ << endl;
 
-	// Вычисляем функцию Эйлера
-	phi = (primeNumberP - 1) * (primeNumberQ - 1);
-	cout << "Phi: " << phi << endl;
+  // Вычисляем функцию Эйлера
+  phi = (primeNumberP - 1) * (primeNumberQ - 1);
+  cout << "Phi: " << phi << endl;
 
-	// Вычисляем экспоненту, часть публичного
-	exponent = calculateExponent(phi);
-	// Вычисляем закрытый ключ
-	numberD = calculateNumberD(exponent, phi);
+  // Вычисляем экспоненту, часть публичного
+  exponent = calculateExponent(phi);
+  // Вычисляем закрытый ключ
+  numberD = calculateNumberD(exponent, phi);
 
-	cout << "Public key pair: {Mod = [" << modulePQ << "] | exp = [" << exponent << "]" << endl;
-	cout << "Private key pair: {Mod = [" << modulePQ << "] | D = [" << numberD << "]" << endl;
+  cout << "Public key pair: {Mod = [" << modulePQ << "] | exp = [" << exponent << "]" << endl;
+  cout << "Private key pair: {Mod = [" << modulePQ << "] | D = [" << numberD << "]" << endl;
 
-	// Шифруем каждый символ текста
-	for (long int i = 0; i < text.length(); i++)
-	{
+  // Шифруем каждый символ текста
+  for (long int i = 0; i < text.length(); i++)
+  {
     encryptedChars.push_back(encrypt(text[i], exponent, modulePQ));
-	}
+  }
 
-	// Выводим зашифрованный текст и сохраняем в файл
+  // Выводим зашифрованный текст и сохраняем в файл
   ofstream encryptedTextFile("out_encrypted.txt");
-	cout << "Encrypted text: ";
-	for (auto i = encryptedChars.begin(); i != encryptedChars.end(); i++)
+  cout << "Encrypted text: ";
+  for (auto i = encryptedChars.begin(); i != encryptedChars.end(); i++)
   {
     cout << char(*i);
     encryptedTextFile << char(*i);
@@ -165,23 +165,23 @@ int main()
 
   cout << endl;
 
-	// Расшифровываем обратно
-	for (long int i = 0; i < encryptedChars.size(); i++)
-	{
+  // Расшифровываем обратно
+  for (long int i = 0; i < encryptedChars.size(); i++)
+  {
     decryptedChars.push_back(decrypt(encryptedChars[i], numberD, modulePQ));
-	}
+  }
 
   // Выводим расшифрованный текст и сохраняем в файл
   ofstream decryptedTextFile("out_decrypted.txt");
   cout << "Decrypted text: ";
-	for (auto i = decryptedChars.begin(); i != decryptedChars.end(); i++)
-	{
+  for (auto i = decryptedChars.begin(); i != decryptedChars.end(); i++)
+  {
     cout << char(*i);
     decryptedTextFile << char(*i);
-	}
+  }
 
   decryptedTextFile.close();
-	cout << endl;
+  cout << endl;
 
   return 0;
 }
