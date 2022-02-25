875da2852b1aeff7e1e257aa0592ec1e84fdf535
developer.izakar.cbox <developer.izakar.cbox@gmail.com>
2022-02-25 16:06:52 +0300

    Indents fix

--- rsa example/rsa_v2.cpp
+++ rsa example/rsa_v2.cpp
@@ -7,92 +7,92 @@ using namespace std;
 
 long int greatestCommonDivisor(long int a, long int b)
 {
-    while (a > 0)
-    {
-        long int tempVal;
+  while (a > 0)
+  {
+    long int tempVal;
 
-        tempVal = a;
-        a = b % a;
-        b = tempVal;
-    }
+    tempVal = a;
+    a = b % a;
+    b = tempVal;
+  }
 
-    return b;
+  return b;
 }
 
 long int calculateExponent(long int phi)
 {
-    long int exponent;
+  long int exponent;
 
-    for (exponent = 2; exponent < phi; exponent++)
+  for (exponent = 2; exponent < phi; exponent++)
+  {
+    if (greatestCommonDivisor(exponent, phi) == 1)
     {
-        if (greatestCommonDivisor(exponent, phi) == 1)
-        {
-            return exponent;
-        }
+      return exponent;
     }
+  }
 
-    return -1;
+  return -1;
 }
 
 long int calculateNumberD(long int &exponent, long int &phi)
 {
-    long int numberD;
-    long int i = 1;
+  long int numberD;
+  long int i = 1;
 
-    while (1)
+  while (1)
+  {
+    i = i + phi;
+
+    if (i % exponent == 0)
     {
-        i = i + phi;
-
-        if (i % exponent == 0)
-        {
-            numberD = i / exponent;
-            return numberD;
-        }
+      numberD = i / exponent;
+      return numberD;
     }
+  }
 }
 
 long int encrypt(long int c, long int &exponent, long int &modulePQ)
 {
-    long int current, result;
+  long int current, result;
 
-    current = c - 96;
-    result = 1;
+  current = c - 96;
+  result = 1;
 
-    for (long int i = 0; i < exponent; i++)
-    {
-        result = result * current;
-        result = result % modulePQ;
-    }
+  for (long int i = 0; i < exponent; i++)
+  {
+    result = result * current;
+    result = result % modulePQ;
+  }
 
-    return result + 96;
+  return result + 96;
 }
 
 long int decrypt(long int c, long int &numberD, long int &modulePQ)
 {
-    long int current, result;
+  long int current, result;
 
-    current = c - 96;
-    result = 1;
+  current = c - 96;
+  result = 1;
 
-    for (long int i = 0; i < numberD; i++)
-    {
-        result = result * current;
-        result = result % modulePQ;
-    }
+  for (long int i = 0; i < numberD; i++)
+  {
+    result = result * current;
+    result = result % modulePQ;
+  }
 
-    return result + 96;
+  return result + 96;
 }
 
 int main()
 {
 	vector<double> encryptedChars;
-	vector<double> decryptedChars;
+  vector<double> decryptedChars;
 	string text;
 	string key;
 
 	long int primeNumberP, primeNumberQ;
 
-    // Произведение P * Q, модуль
+  // Произведение P * Q, модуль
 	long int modulePQ;
 	// Функция Эйлера
 	long int phi;
@@ -125,11 +125,11 @@ int main()
 
 		keyFile.close();
 
-        cout << "PrimeP: " << primeNumberP << endl;
-        cout << "PrimeQ: " << primeNumberQ << endl;
+    cout << "PrimeP: " << primeNumberP << endl;
+    cout << "PrimeQ: " << primeNumberQ << endl;
 	}
 
-    // Вычисляем публичный и закрытый ключ
+  // Вычисляем публичный и закрытый ключ
 	// Вычисляем произведение P * Q
 	modulePQ = primeNumberP * primeNumberQ;
 	cout << "Mod: " << modulePQ << endl;
@@ -149,39 +149,39 @@ int main()
 	// Шифруем каждый символ текста
 	for (long int i = 0; i < text.length(); i++)
 	{
-        encryptedChars.push_back(encrypt(text[i], exponent, modulePQ));
+    encryptedChars.push_back(encrypt(text[i], exponent, modulePQ));
 	}
 
 	// Выводим зашифрованный текст и сохраняем в файл
-    ofstream encryptedTextFile("out_encrypted.txt");
+  ofstream encryptedTextFile("out_encrypted.txt");
 	cout << "Encrypted text: ";
 	for (auto i = encryptedChars.begin(); i != encryptedChars.end(); i++)
-    {
-        cout << char(*i);
-        encryptedTextFile << char(*i);
-    }
+  {
+    cout << char(*i);
+    encryptedTextFile << char(*i);
+  }
 
-    encryptedTextFile.close();
+  encryptedTextFile.close();
 
-    cout << endl;
+  cout << endl;
 
 	// Расшифровываем обратно
 	for (long int i = 0; i < encryptedChars.size(); i++)
 	{
-        decryptedChars.push_back(decrypt(encryptedChars[i], numberD, modulePQ));
+    decryptedChars.push_back(decrypt(encryptedChars[i], numberD, modulePQ));
 	}
 
-    // Выводим расшифрованный текст и сохраняем в файл
-    ofstream decryptedTextFile("out_decrypted.txt");
-    cout << "Decrypted text: ";
+  // Выводим расшифрованный текст и сохраняем в файл
+  ofstream decryptedTextFile("out_decrypted.txt");
+  cout << "Decrypted text: ";
 	for (auto i = decryptedChars.begin(); i != decryptedChars.end(); i++)
 	{
-	    cout << char(*i);
-        decryptedTextFile << char(*i);
+    cout << char(*i);
+    decryptedTextFile << char(*i);
 	}
 
-    decryptedTextFile.close();
+  decryptedTextFile.close();
 	cout << endl;
 
-    return 0;
+  return 0;
 }
